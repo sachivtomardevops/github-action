2237aad3d67a60c1a14c22fb827b700084d14ed8
sachivtomardevops <sachivtomardevops@gmail.com>
2026-08-11 10:27:51 +0530

    ek aur nya

--- parent/main.tf
+++ parent/main.tf
@@ -1,32 +1,32 @@
 module "sigma" {
   source = "../child/resource"
-  kaka = var.kaka
+  kaka   = var.kaka
 
 }
 
 module "vnet" {
-  source = "../child/vnet"
-  kantivnet = var.kantivnet
+  source     = "../child/vnet"
+  kantivnet  = var.kantivnet
   depends_on = [module.sigma]
 }
 
 module "subnet" {
-  source = "../child/subnet"
+  source      = "../child/subnet"
   kantisubnet = var.kantisubnet
-  depends_on = [module.vnet]
+  depends_on  = [module.vnet]
 }
 
 module "pip" {
-  source = "../child/PIP"
-  kantipip = var.kantipip
+  source     = "../child/PIP"
+  kantipip   = var.kantipip
   depends_on = [module.subnet]
 }
 
 module "nic" {
   source = "../child/NIC"
 
-  kantinic = var.kantinic
-  depends_on = [module.pip] 
+  kantinic   = var.kantinic
+  depends_on = [module.pip]
 }
-  
+
 
--- parent/provider.tf
+++ parent/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"   # Ya strictly non-buggy version like ">= 3.116.0" / ">= 4.2.0"
+      version = ">= 3.0" # Ya strictly non-buggy version like ">= 3.116.0" / ">= 4.2.0"
     }
   }
 }
