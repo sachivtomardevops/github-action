8d1cec8975013af08682cfd3b0a40d41a6dfccd9
sachivtomardevops <sachivtomardevops@gmail.com>
2026-08-11 10:22:35 +0530

    nya code1

--- parent/provider.tf
+++ parent/provider.tf
@@ -1,13 +1,13 @@
 terraform {
- required_providers {
-   azurerm = {
-     source = "hashicorp/azurerm"
-     version = "=4.0"
-   }
- }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"   # Ya strictly non-buggy version like ">= 3.116.0" / ">= 4.2.0"
+    }
+  }
 }
-provider "azurerm" {
- features {}
-subscription_id = "37189ad6-207d-4d0d-bf9e-0dd7c974b26f"
 
+provider "azurerm" {
+  features {}
+  subscription_id = "37189ad6-207d-4d0d-bf9e-0dd7c974b26f"
 }
